c80e2d8364e23f42c259cb1543eb9f8be44abca1
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-04-12 22:36:25 -0400

    completed loop code along

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+let names = [];
+
+function writeCards(names){
+    let thankYou = [];
+for (let i = 0; i < names.length; i++ ){
+    thankYou[i] =`Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    debugger;
+    console.log(thankYou)
+}
+return thankYou;
+}
+writeCards(names);
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+function countDown(num){ 
+    let i = num;
+    for (i = num; i >=0; i--){
+        console.log(i)
+    }
+
+  }
+
